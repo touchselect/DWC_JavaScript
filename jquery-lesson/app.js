258c3ef8a9a82387cc734131c8d5b35dd3dda01b
pharmile <nanaseprg@gmail.com>
2023-05-03 01:04:11 +0900

    jquery

---
+++ jquery-lesson/app.js
@@ -0,0 +1,31 @@
+// $(function () {
+// 	$('.box1').css({
+// 		'background-color': '#0000ff',
+// 		'height': '100px'
+// 	});
+// });
+
+// $(function () {
+// 	$('.box1').slideDown();
+// });
+// $(function () {
+// 	$('.box1').slideUp();
+// });
+
+// $(function () {
+// 	$('.box1').show();
+// 	$('.box1').css({ 'background-color': '#0000ff' });
+// });
+// $(function () {
+// 	$('.box1').hide();
+// });
+
+$(function (){
+	$('.box1').slideDown(function (){
+		$('.box1').css({
+		'background-color': '#0000ff',
+		'width': '200px',
+		'height': '100px'
+		}).slideUp();
+	});
+});
