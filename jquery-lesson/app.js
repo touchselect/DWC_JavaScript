2206da620929e53f7383881582f32a1f1b7e1863
pharmile <nanaseprg@gmail.com>
2023-05-06 10:58:29 +0900

    this children

--- jquery-lesson/app.js
+++ jquery-lesson/app.js
@@ -39,11 +39,38 @@
 // 	});
 // });
 
+// $(function () {
+// 	$('.box1').on('click', function () {
+// 		$('.box1').addClass('box1-ext');
+// 	})
+// 	$('.box1').mouseout(function () {
+// 		$('.box1').removeClass('box1-ext');
+// 	});
+// });
+
+// $(function () {
+// 	$('.bg1').on('click', function () {
+// 		$('.bg1').slideUp();
+// 	});
+// 	$('.bg2').on('click', function () {
+// 		$('.bg2').slideUp();
+// 	});
+// 	$('.bg3').on('click', function () {
+// 		$('.bg3').slideUp();
+// 	});
+// 	$('.bg4').on('click', function () {
+// 		$('.bg4').slideUp();
+// 	});
+// });
+
 $(function () {
 	$('.box1').on('click', function () {
-		$('.box1').addClass('box1-ext');
-	})
-	$('.box1').mouseout(function () {
-		$('.box1').removeClass('box1-ext');
+		$(this).slideUp();
+	});
+});
+
+$(function () {
+	$('button').on('click', function () {
+		$('ul').children().css('color', 'red');
 	});
 });
